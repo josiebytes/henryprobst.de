4138b985f6d1456dd5ed8081774ab5080109ee1e
henryprobst0-design <henryprobst0@gmail.com>
2025-12-16 15:32:54 +0100

    Update kontakt.vue

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -12,7 +12,7 @@ useHead({
     <div class="prose">
       <h1 class="text-center">Kontakt</h1>
 
-      <img src="https://media.licdn.com/dms/image/v2/D4E03AQHPewHFiaVW0A/profile-displayphoto-crop_800_800/B4EZnJ05QgIoAI-/0/1760027710905?e=1767225600&v=beta&t=kYb7JCK7deiGlnNTsLDChy25egm2yV2hyVHWfNvZFxk" alt="Henry Probst">
+      <img src="https://i.postimg.cc/P5RY7Xyn/1760027710905-(1).jpg" alt="Henry Probst">
 
       <div class="cards max-w-[300px] mx-auto">
         <div class="contact | flex items-center gap-3 mb-5">
