382c0ac37fb281da5ce542ae478ad895877e7750
henryprobst0-design <henryprobst0@gmail.com>
2025-12-16 01:06:17 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -13,18 +13,9 @@ useHead({
       <h1 class="text-center">Über mich</h1>
       <p>
         Mein Name ist Henry Probst, ich bin 17 Jahre alt und absolviere derzeit meine duale Ausbildung zum IT-Systemelektroniker bei Technogroup/Evernex und besuche Berufsschulunterricht an der Friedrich-Ebert-Schule in Wiesbaden.
-      <p></p>
-      In meinem beruflichen Alltag beschäftige ich mich intensiv mit Troubleshooting, Hardwarewartung sowie dem Kundensupport im Serverumfeld.
+      </p>
+      <p>In meinem beruflichen Alltag beschäftige ich mich intensiv mit Troubleshooting, Hardwarewartung sowie dem Kundensupport im Serverumfeld.</p>
       <p></p>
         Zu meinen Tätigkeiten gehört die Arbeit an professionellen Serversystemen wie HP ProLiant und Dell PowerEdge, sowohl bei der Fehlerdiagnose als auch bei Wartungs- und Austauschprozessen direkt beim Kunden.
-      <p></p>
-        <p>Dabei lege ich großen Wert auf zuverlässige Lösungen, saubere Dokumentation und einen professionellen Umgang mit Kunden.
-      </p>
-      <hr>
-      <p>Auch privat bin ich mit großer Begeisterung in der IT-Welt unterwegs.
-Besonders das Arbeiten mit IT-Hardware, das Analysieren technischer Probleme und das Optimieren von Systemen gehören zu meinen Interessen.
-Diese Leidenschaft motiviert mich, mich kontinuierlich weiterzubilden und mein technisches Know-how stetig auszubauen.</p>
-    </div>
-  </div>
-</section>
-</template>
+      <p>
+        Dabei lege ich großen Wert auf zuverlässige Lösungen, saubere Dokumentation und einen professionellen Umgang mit Kunden.
