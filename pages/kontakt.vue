82d799be9a55feec7479cbe09636031cefda4916
henryprobst0-design <henryprobst0@gmail.com>
2025-12-16 16:14:43 +0100

    Update kontakt.vue

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -18,13 +18,13 @@ useHead({
 
       <br>
 
-      <div class="cards max-w-[300px] mx-auto">
-        <div class="contact | flex items-center">
-          <div class="i-mdi-gmail w-6 h-6 bg-red-500"></div>
+      <div class="cards max-w-[300px] mx-auto", align="center">
+        <div class="contact | flex items-center", align="center">
+          <div class="i-mdi-gmail w-6 h-6 bg-red-500", align="center"></div>
           <a href="mailto:kontakt@henryprobst.de">kontakt@henryprobst.de</a>
         </div>
-        <div class="contact | flex items-center">
-          <div class="i-mdi-linkedin w-6 h-6 bg-blue-500"></div>
+        <div class="contact | flex items-center", align="center">
+          <div class="i-mdi-linkedin w-6 h-6 bg-blue-500, align="center""></div>
           <a href="https://linkedin.com/in/henry-probst-1b8420389" target="_blank">Henry Probst</a>
         </div>
       </div>
