0d9efd1e39a7f4ecdaa37c877199330466d25f50
henryprobst0-design <henryprobst0@gmail.com>
2025-12-16 01:00:47 +0100

    Clean up empty paragraph tags in index.vue
    Removed empty paragraph tags and cleaned up spacing.

--- pages/index.vue
+++ pages/index.vue
@@ -13,11 +13,11 @@ useHead({
       <h1 class="text-center">Über mich</h1>
       <p>
         Mein Name ist Henry Probst, ich bin 17 Jahre alt und absolviere derzeit meine duale Ausbildung zum IT-Systemelektroniker bei Technogroup/Evernex und besuche Berufsschulunterricht an der Friedrich-Ebert-Schule in Wiesbaden.
-      </p>
+      <p></p>
       <p>In meinem beruflichen Alltag beschäftige ich mich intensiv mit Troubleshooting, Hardwarewartung sowie dem Kundensupport im Serverumfeld.</p>
-      </p>
+      <p></p>
         Zu meinen Tätigkeiten gehört die Arbeit an professionellen Serversystemen wie HP ProLiant und Dell PowerEdge, sowohl bei der Fehlerdiagnose als auch bei Wartungs- und Austauschprozessen direkt beim Kunden.
-      <p>
+      <p></p>
         Dabei lege ich großen Wert auf zuverlässige Lösungen, saubere Dokumentation und einen professionellen Umgang mit Kunden.
       </p>
       <hr>
