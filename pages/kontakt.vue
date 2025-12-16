c39a196b72c3d180c6895410cde68159e1c9ed59
henryprobst0-design <henryprobst0@gmail.com>
2025-12-16 15:35:41 +0100

    Update kontakt.vue

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -12,7 +12,7 @@ useHead({
     <div class="prose">
       <h1 class="text-center">Kontakt</h1>
 
-      <img src="https://i.postimg.cc/P5RY7Xyn/1760027710905-(1).jpg" alt="Henry Probst"> style="vertical-align: middle;"/>
+      <img src="https://i.postimg.cc/P5RY7Xyn/1760027710905-(1).jpg" alt="Henry Probst">
 
       <div class="cards max-w-[300px] mx-auto">
         <div class="contact | flex items-center gap-3 mb-5">
